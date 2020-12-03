1e72897b64077dbd2cf23aac85d2c9d48ef90d04
booleancube <ashleyowenspt@gmail.com>
2020-12-03 12:22:41 -0700

    Fixed opening move bug and documented GameBoard code.

--- GameBoard.py
+++ GameBoard.py
@@ -1,31 +1,55 @@
-# Minesweeper game board with primary game logic
+# Author: Ashley Owens
+# Date: 12/7/2020
+# Description: CS 325, Portfolio Project
+# Minesweeper board class contains primary game logic.
 
 from BoardTiles import Tiles
 import random
 
 class Board:
-
-    def __init__(self):
-        self.rows = 8
-        self.cols = 8
-        self.mines = 10
+    """
+    Contains Minesweeper game logic.
+    """
+    def __init__(self, rows, cols, mines):
+        """
+        Initializes minesweeper game and board attributes.
+        Args:
+            rows (int): number of rows
+            cols (int): number of columns
+            mines (int): number of mines
+        """
+        self.rows = rows
+        self.cols = cols
+        self.mines = mines
         self.opened = 0
         self.game_won = False
         self.game_lost = False
-        self.board = False
-        self.tiles = False
+        self.board = self.__init__board__()
+        self.tiles = self.__init__tiles__()
 
-        if self.board is False:
-            mines = random.sample(range(0, self.rows * self.cols), self.mines)
-            row = (
-                lambda i, j: Tiles.mine
-                if i * self.cols + j in mines
-                else Tiles.zero
-            )
-            self.board = [[Tiles(i, j, row(i, j)) for j in range(self.cols)] for i in range(self.rows)]
+    def __init__board__(self):
+        """
+        Initializes game board placing mines in random locations.
+        Returns:
+            matrix: game board matrix filled with mines
+        """
+        mines = random.sample(range(0, self.rows * self.cols), self.mines)
+        row = (lambda i, j: Tiles.mine if i * self.cols + j in mines else Tiles.zero)
+        return [[Tiles(i, j, row(i, j)) for j in range(self.cols)] for i in range(self.rows)]
 
-        if self.tiles is False:
-            self.tiles = [[Tiles(i, j, Tiles.closed) for j in range(self.cols)] for i in range(self.rows)]
+    def __init__tiles__(self):
+        """
+        Initializes matrix of game tiles using Tiles class.
+        """
+        return [[Tiles(i, j, Tiles.closed) for j in range(self.cols)] for i in range(self.rows)]
+    
+    def __str__(self):
+        """
+        Useful for debugging and printing game board to the console
+        Returns:
+            string: game board
+        """
+        return "\n".join(["  ".join([f"{str(tile):2}" for tile in row]).rstrip() for row in self.tiles])
     
     def get_board(self):
         return self.board
@@ -34,6 +58,15 @@ class Board:
         self.__init__()
 
     def open_tile(self, i, j):
+        """
+        Primary game logic for opening a tile. Uses helper methods and performs tile validation, mine 
+        redistribution for opening move, tracking opened tiles, and recursively opening adjacent tiles. 
+        Args:
+            i (int): user selected row
+            j (int): user selected column
+        Returns:
+            list: opened tile objects 
+        """
         if self.game_lost or not self.valid_tile(i, j):
             return []
 
@@ -43,6 +76,7 @@ class Board:
         if self.game_won:
             return []
 
+        # Redistributes mine field and numbers tiles for the first move of the game.
         if self.opened == 0:
             self.adjust_tiles(i, j)
             self.number_tiles()
@@ -50,25 +84,39 @@ class Board:
         self.opened += 1
         self.tiles[i][j] = self.board[i][j]
 
+        # Checks for game ending moves.
         if (self.opened + self.mines) == (self.rows * self.cols):
             self.game_won = True
-
         if self.tiles[i][j].category == Tiles.mine:
             self.game_lost = True
 
+        # Opens adjacent tiles as needed.
         elif self.tiles[i][j].number == 0:
             return self.open_adjacents(i, j, [self.tiles[i][j]])
 
         return [self.tiles[i][j]]
 
-
     def valid_tile(self, i, j):
+        """
+        Ensures row, col coordinates are valid game board moves.
+        Args:
+            i (int): user selected row
+            j (int): user selected column
+        Returns:
+            boolean: True if valid else False
+        """
         if (i >= 0 and i < self.rows) and (j >= 0 and j < self.cols):
             return True
         return False
-
     
     def adjust_tiles(self, row, col):
+        """
+        Helper method used only for the opening move to randomly reallocate mine field.
+        Args:
+            row (int): user selected row
+            col (int): user selected column
+        """
+        # Iterates through the user selected 3x3 grid.
         for i in [row+1, row-1, row]:
             for j in [col+1, col-1, col]:
 
@@ -77,9 +125,8 @@ class Board:
                     random_i = random.randint(0, self.rows - 1)
                     random_j = random.randint(0, self.cols - 1)
 
-                    # Search for a valid location on the game board to place the mine.
-                    while self.board[random_i][random_j].category == Tiles.mine:
-                    # or (abs(row-random_i) <= 1 and abs(col-random_j) <= 1):
+                    # Searches for locations to place the mine outside of the 3x3 starting grid.
+                    while self.board[random_i][random_j].category == Tiles.mine or (abs(row-random_i) <= 1 and abs(col-random_j) <= 1):
                         random_i = random.randint(0, self.rows - 1)
                         random_j = random.randint(0, self.cols - 1)
                     
@@ -88,25 +135,42 @@ class Board:
                     self.board[i][j] = Tiles(i, j, Tiles.zero)
     
     def number_tiles(self):
+        """
+        Iterates through game board enumerating tiles according to their proximity to mines.
+        """
+        # Iterates through entire game board.
         for row in range(self.rows):
             for col in range(self.cols):
                 if self.board[row][col].category == Tiles.mine:
                     continue
                 adjacent_mines = 0
 
+                # Calculates number of mines for each 3x3 grid.
                 for i in [row+1, row-1, row]:
                     for j in [col+1, col-1, col]:
                         if (self.valid_tile(i, j) and self.board[i][j].category == Tiles.mine):
                             adjacent_mines += 1
                 
+                # Sets each game board tile's mine proximity number.
                 self.board[row][col] = Tiles(row, col, str(adjacent_mines))
 
     def open_adjacents(self, row, col, opened_tile):
+        """
+        Uses recursion to open adjacent tiles.
+        Args:
+            row (int): user selected row
+            col (int): user selected column
+            opened_tile (obj): tile object
+        Returns:
+            list: opened tile objects
+        """
+        # Doesn't open adjacent mines.
         if self.valid_tile(row, col):
             if self.board[row][col] == Tiles.mine:
                 self.tiles[row][col] = self.board[row][col]
                 return [Tiles(row, col, Tiles.mine)]
             
+            # Iterates through 3x3 grid, only opening closed tiles adjacent to a zero tile.
             for i in [row+1, row-1, row]:
                 for j in [col+1, col-1, col]:
                     if (self.valid_tile(i, j) and self.tiles[i][j].category == Tiles.closed):
@@ -114,15 +178,21 @@ class Board:
                         self.tiles[i][j] = self.board[i][j]
                         opened_tile.append(self.board[i][j])
 
+                        # Checks for a game winning move while opening adjacent tiles.
                         if (self.opened + self.mines) == (self.rows * self.cols):
                             self.game_won = True
 
+                        # If an adjacent tile is zero, recursively opens that tile's adjacent tiles.
                         if self.board[i][j].category == Tiles.zero:
                             self.open_adjacents(i, j, opened_tile)
         return opened_tile
 
-
     def get_mines(self):
+        """
+        Helper method for obtaining current minefield.
+        Returns:
+            list: all mine coordinates in an (i, j) tuple
+        """
         mines = []
         for i in range(self.rows):
             for j in range(self.cols):
@@ -132,19 +202,12 @@ class Board:
 
 
 
-    def __str__(self):
-        return "\n".join(
-            [
-                "  ".join([f"{str(tile):2}" for tile in row]).rstrip()
-                for row in self.tiles
-            ]
-        )
 
-game = Board()
+# game = Board(8, 8, 10)
 # print(game.tiles[0][0].category)
-print('Opening move')
-print(game)
-game.open_tile(2, 2)
+# print('Opening move')
+# print(game)
+# game.open_tile(2, 2)
 # print(game)
 # print("second move")
 # game.open_tile(2, 3)
--- Minesweeper.py
+++ Minesweeper.py
@@ -1,7 +1,7 @@
 # Author: Ashley Owens
 # Date: 12/7/2020
 # Description: CS 325, Portfolio Project
-# Uses PyGame to implement Minesweeper.
+# Uses PyGame GUI to implement Minesweeper.
 
 try:
     import sys, os, pygame
@@ -17,8 +17,13 @@ except ImportError:
 
 def loadImage(name):
     """
-    :param name: filename to be loaded (string)
-    :return: image object, rectangular area of the image object
+    Loads an image for blitting.
+    Args:
+        name(string): filename to be loaded
+    Raises:
+        SystemExit: if image not found
+    Returns:
+        image: blittable image object
     """
     fullname = os.path.join('images', name)
     try:
@@ -30,23 +35,37 @@ def loadImage(name):
     except FileNotFoundError:
         print('Cannot load image: ', fullname)
         raise SystemExit
+
     return image
 
 
-
 def initializeScreen(x, y):
-    # Initializes the PyGame screen and background.
+    """
+    Initializes the Pygame surface for blitting game elements.
+    Args:
+        x (int): screen width
+        y (int): screen height
+    Returns:
+        object: Pygame surface object
+    """
+    # Initializes the PyGame screen.
     pygame.init()
     screen = pygame.display.set_mode((x, y))
     icon = loadImage('gameicon.png')
     pygame.display.set_icon(icon)
     pygame.display.set_caption('Minesweeper')
-    coordinates = drawBoard(screen)
-    return screen, coordinates
+    return screen
 
 
 def drawBoard(screen):
-
+    """
+    Initializes the game board filled with closed tiles of alternating colors
+    while generating a dictionary of rectangle locations for tracking user clicks.
+    Args:
+        screen (object): Pygame surface
+    Returns:
+        dict: key = (row, col), value = associated rectangle object
+    """
     # Uses itertools to alternate tile colors
     colors = cycle((tile_color1, tile_color2))
     coordinates = {}
@@ -54,17 +73,26 @@ def drawBoard(screen):
     for row in range(rows):
         next(colors)
 
+        # Creates a rectangle object for each position on game board and saves it in a dictionary
         for col in range(cols):
-            # Creates a rectangle object for each position on game board and saves it in a dictionary
             current = Rect(60*row, 60*col, boxX, boxY)
             coordinates[col, row] = current
 
             # Draws the tiles to the screen.
             pygame.draw.rect(screen, next(colors), current, 0)
+
     return coordinates
 
-def makeMove(screen, coordinates, key):
 
+def makeMove(screen, coordinates, key):
+    """
+    Given user selected coordinates, opens tiles and calls helper functions to display 
+    results of a given move.
+    Args:
+        screen (object): Pygame surface object
+        coordinates (dict): key = (row, col), value = associated rectangle object
+        key (tuple): row, col coordinates of user selection
+    """
     # Stores the row, col coordinates of the desired move.
     i, j = key
 
@@ -72,54 +100,78 @@ def makeMove(screen, coordinates, key):
     tiles = board.open_tile(i, j)
     removeTile(screen, coordinates, tiles)
 
+    # Displays Game Over image
+    if board.game_lost == True:
+        gameOver(screen, loadImage('game-over.png'))
+
+    # Displays Game Won image
+    elif board.game_won == True:
+        gameOver(screen, loadImage('trophy.png'))
+       
+
+def gameOver(screen, image):
+    """
+    Helper function to display game over messages
+    Args:
+        screen (object): Pygame surface object
+        image (.PNG): image
+    """
+    image_rect = ((x/2-60), (y/2-60))
+    screen.blit(image, image_rect)
+
 
 def removeTile(screen, coordinates, tiles):
-    
+    """
+    Opens tile objects to reveal checkerboard background, tile numbers, and mines.
+    Args:
+        screen (object): Pygame surface object
+        coordinates (dictionary): key = (row, col), value = associated rectangle object
+        tiles (list): list of tile objects to be opened on the game board.
+    """
     for tile in tiles:
         # Stores the current rect object associated with the tile's coordinates.
         row, col = tile.row, tile.col
         category = tile.category
         current = coordinates[row, col]
 
+        # Alternates opened tiles background color
         if (row + col) % 2 == 0:
             pygame.draw.rect(screen, bg_color1, current, 0)
-
         else:
             pygame.draw.rect(screen, bg_color2, current, 0)
         
-        
+        # Displays mines
         if category == "x":
             image_rect = (8+(60*col), 5+(60*row))
             screen.blit(loadImage(category +'.png'), image_rect)
         
-        
+        # Displays the tile's number
         elif int(category) > 0:
             image_rect = (16+(60*col), 17+(60*row))
             screen.blit(loadImage(category +'.png'), image_rect)
 
-        if board.game_lost == True:
-            image_rect = ((x/2-60), (y/2-60))
-            screen.blit(loadImage('game-over.png'), image_rect)
-    
-        elif board.game_won == True:
-            image_rect = ((x/2-60), (y/2-60))
-            screen.blit(loadImage('trophy.png'), image_rect)
-
 
 def placeFlag(screen, key):
+    """
+    Places a flag in the center of a selected rectangle object. 
+    Used for marking suspected mine locations.
+    Args:
+        screen (object): Pygame surface object
+        key (tuple): row, col of flag location
+    """
     row, col = key
     flag_img = loadImage("flag.png")
     image_rect = (16+(60*col), 15+(60*row))
     screen.blit(flag_img, image_rect)
-    
-
-def blitText(screen, category, rect):
-    font = pygame.font.SysFont("ariel", 15)
-    label = font.render(category, True, blue)
-    screen.blit(label, rect)
 
 
 def showBoard(screen, coordinates):
+    """
+    Enables all tiles to be uncovered and displayed.
+    Args:
+        screen (object): Pygame surface
+        coordinates (dict): key = (row, col), value = associated rectangle object
+    """
     colors = cycle((bg_color1, bg_color2))
     mine_img = loadImage("x.png")
 
@@ -135,67 +187,81 @@ def showBoard(screen, coordinates):
             if category == "x":
                 image_rect = (8+(60*col), 5+(60*row))
                 screen.blit(mine_img, image_rect)
+
+
+def blitText(screen, category, rect):
+    """
+    Helper function for blitting text to the screen.
+    Args:
+        screen (object): Pygame surface
+        category (string): text to display.
+        rect (object): rectangle object or location 
+    """
+    font = pygame.font.SysFont("ariel", 15)
+    label = font.render(category, True, tile_color1)
+    screen.blit(label, rect)
         
 
 def main():
     """
-    Initializes game board screen and runs game loop.
+    Runs game loop.
     """
-    screen, coordinates = initializeScreen(x, y)
-    clicks = []
-    print(board.get_mines())
-
-    # Initiates the game loop.
     while True:
-        
         pygame.display.flip()
         
-
         # Clicking on window exit button ends game.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
 
-            # Locates click coordinates in dict and adds red outline to enhance footprint.
+            # Locates click coordinates in dictionary.
             if event.type == pygame.MOUSEBUTTONDOWN:
 
-                # Checks for clicks in order to removes tiles.
+                # Left click removes tiles using coordinates dictionary.
                 if event.button == 1:
-                
                     for key, value in coordinates.items():
                         if value.collidepoint(event.pos):
-                            clicks.append(key)
-                            makeMove(screen, coordinates, key)
+                            
+                            # Allows user to open tiles when game is lost to view mine locations.
+                            if board.game_lost is True:
+                                i, j = key
+                                removeTile(screen, coordinates, [board.get_board()[i][j]])
+                            
+                            # Opens tiles to play the game.
+                            else:
+                                makeMove(screen, coordinates, key)
+
                             pygame.display.update()
                             
 
-                # Checks for right clicks to place a flag.
+                # Right click places a flag in suspected mine locations.
                 if event.button == 3:
                     for key, value in coordinates.items():
                         if value.collidepoint(event.pos):
                             placeFlag(screen, key)
                             pygame.display.update()
                             print("The mines are located at: ", board.get_mines())
-                            # print(key)
-
-                # print(clicks)
-        
-
-
 
 
 if __name__ == '__main__':
-    board = Board()
+    # Initializes screen and game board dimensions.
     rows, cols = 8, 8
+    mines = 10
     x, y = 480, 480
-    boxX = x/8
-    boxY = y/8
+    boxX, boxY = x/rows, y/cols
+
+    # Initializes game board object.
+    board = Board(rows, cols, mines)
+
+    # Initializes game board colors.
     bg_color1 = (184, 147, 92)
     bg_color2 = (150, 113, 57)
     tile_color1 = (144, 207, 63)
     tile_color2 = (176, 233, 102)
-    white = (255, 255, 255)
-    blue = (0, 0, 225)
-    
 
+    # Initializes Pygame surface, blits game board, stores rect objects.
+    screen = initializeScreen(x, y)
+    coordinates = drawBoard(screen)
+
+    # Runs the game loop.
     main()
